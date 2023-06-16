f82544400568bffba45a43e97e370057ca0bd0ac
PallvaKuarweb23 <pallavamarc@gmail.com>
2023-06-16 15:10:49 +0530

    portfolio2

--- README.md
+++
@@ -1,3 +0,0 @@
-# ❤️ Pallava Kuar's Portfolio 🙏
-### _This is my Portfolio Project, I made it using HTML, CSS and Javascript.
-
---
+++ index.js
@@ -0,0 +1,23 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.getElementById('resume-button-1').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1up4dMx7FQMzTD5ihQMngZcX6sz3QiSQS/view?usp=sharing", "_blank");
+});
+
+document.getElementById('resume-button-2').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1up4dMx7FQMzTD5ihQMngZcX6sz3QiSQS/view?usp=sharing", "_blank");
+});
+
